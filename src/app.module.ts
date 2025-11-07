d29cb6b4fe27e7664272d4efeaf795300225adde
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-11-07 11:38:22 +0200

    Lesson 5 /connectDB / migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'node:path';
 
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SourceCode } from 'eslint';
+
+import { DatabaseConfig } from './configs/config.type';
 import configuration from './configs/configuration';
 import { ArticlesModule } from './modules/articles/articles.module';
 import { CommentsModule } from './modules/comments/comments.module';
@@ -13,15 +17,29 @@ import { UsersModule } from './modules/users/users.module';
       load: [configuration],
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'project-nestjs',
-      entities: [],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const config = configService.get<DatabaseConfig>('database');
+        return {
+          type: 'postgres',
+          host: config?.host,
+          port: config?.port,
+          username: config?.user,
+          password: config?.password,
+          database: config?.name,
+          entities: [
+            path.join(
+              process.cwd(),
+              'dist',
+              'database',
+              'entities',
+              '*.entity.js',
+            ),
+          ],
+          synchronize: false,
+        };
+      },
+      inject: [ConfigService],
     }),
     ArticlesModule,
     UsersModule,
